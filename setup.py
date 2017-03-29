da42e47e7f04b93a56f3012e1bf80296ff39c2a0
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2017-03-29 00:49:01 -0400

    bump 1.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='amcrest',
-      version='1.1.5',
+      version='1.1.6',
       description='Python wrapper implementation for Amcrest cameras.',
       author='Douglas Schilling Landgraf, Marcelo Moreira de Mello',
       author_email='dougsland@gmail.com, tchello.mello@gmail.com',
