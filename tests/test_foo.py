14b9061097fde9a7e5d7ae410dd4082a35837224
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2017-03-28 22:03:01 -0400

    Deleted test_foo.py

--- tests/test_foo.py
+++
@@ -1,9 +0,0 @@
-"""Tests for Amcrest module."""
-import unittest
-
-
-class TestAmcrest(unittest.TestCase):
-    """Tests for Amcrest basic"""
-
-    def test_foo(self):
-        self.assertEqual(1, 1)
