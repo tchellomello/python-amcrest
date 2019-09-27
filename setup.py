551dfcaa5a83a3b6b1a10579500356378758a81f
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2019-09-26 22:43:55 -0400

    Bumped version to 1.5.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 
 setup(name='amcrest',
-      version='1.5.3',
+      version='1.5.4',
       description='Python wrapper implementation for Amcrest cameras.',
       long_description=readme(),
       author='Douglas Schilling Landgraf, Marcelo Moreira de Mello',
